bea9849c1a7d645e772ad0c898c4b883d1b75ea4
jo <160404609+Jo2410@users.noreply.github.com>
2025-12-06 01:15:17 +0200

    add product repository

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import {
+  BrandModel,
+  BrandRepository,
+  CategoryModel,
+  CategoryRepository,
+  ProductModel,
+} from 'src/DB';
+import { ProductRepository } from 'src/DB/repository/product.repository';
+import { S3Service } from 'src/common';
 
 @Module({
+  imports: [CategoryModel, ProductModel, BrandModel],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [
+    ProductService,
+    ProductRepository,
+    BrandRepository,
+    CategoryRepository,
+    S3Service,
+  ],
 })
 export class ProductModule {}
