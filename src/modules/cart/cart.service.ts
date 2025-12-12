4ed77636e77017c3ba41a34e26a66084d9e21942
jo <160404609+Jo2410@users.noreply.github.com>
2025-12-12 22:49:13 +0200

    add cart module

---
+++ src/modules/cart/cart.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCartDto } from './dto/create-cart.dto';
+import { UpdateCartDto } from './dto/update-cart.dto';
+import { ProductRepository } from 'src/DB/repository/product.repository';
+import { CartRepository } from 'src/DB';
+
+@Injectable()
+export class CartService {
+  constructor(
+    private readonly productRepository:ProductRepository,
+    private readonly cartRepository:CartRepository,
+  ){}
+  create(createCartDto: CreateCartDto) {
+    return 'This action adds a new cart';
+  }
+
+  findAll() {
+    return `This action returns all cart`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} cart`;
+  }
+
+  update(id: number, updateCartDto: UpdateCartDto) {
+    return `This action updates a #${id} cart`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} cart`;
+  }
+}
